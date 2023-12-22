730831c9c239887c1932560fe8fe4f16a362433b
Celine Natalie <oyaoka.exe@gmail.com>
2023-12-22 23:35:34 +0800

    Modified Routing
    Shifted over TextDisplay func to ExerciseLayout and modified the routing for each exercise and challenge page
    
    Added "Next Question" and "Previous Question" as well to allow users to navigate through the questions

--- fourbetter-app/src/App.js
+++ fourbetter-app/src/App.js
@@ -1,7 +1,7 @@
-import TextDisplay from './components/TextDisplay.js';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import LoginPage from './pages/LogIn';
 import Homepage from './pages/Homepage';
 import SavedWords from './pages/Savedwords';
 import Accountpage from './pages/Accountpage';
@@ -14,13 +14,13 @@ import Endingpage from './pages/endingpage';
 import TestExercise from './components/ExerciseLayout';
 import TestChallenge from './components/ChallengeLayout';
 
-
 function App() {
   return (
     <div>
       <p><Navbar /></p>
         <Routes>
           <Route path="/" exact element={<Homepage />}/>
+          <Route path="/login" exact element= {<LoginPage />}/>
           <Route path="/savedwords" exact element={<SavedWords />}/>
           <Route path="/account" exact element={<Accountpage />}/>
           <Route path="/settings" exact element={<Settingpage/>}/>
--- fourbetter-app/src/components/ChallengeLayout.js
+++ fourbetter-app/src/components/ChallengeLayout.js
@@ -1,23 +1,81 @@
 import { useParams } from 'react-router-dom';
 
+
 function ChallengeLayout(){
-    const {textID} = useParams();
-    const questions = ["This is question 1","This is question 2","This is question 3."];
-    const {challengeNum} = useParams();
+    const {textID, challengeNum} = useParams();
+    const questions = ["This is question 1","This is question 2","This is question 3.", "This is question 4.", "This is question 5."];
+
+    console.log('TextID');
+    console.log(textID);
+    console.log("challengeNum");
+    console.log(challengeNum);
+    console.log(challengeNum == 1)
+
+    if(challengeNum == 1){
+    const nextchallengeNum = parseInt(challengeNum) + 1;
+    console.log(nextchallengeNum);
     return (
         <div>
             <h2>This is the challenges layout number {challengeNum} for text {textID}</h2>
-            <p>Right now, all you can do is see the questions as a list.</p>
+            <p>Right now, all you can do is see the questions</p>
             <br/>
-            {questions.map(qn => (
-                <li>{qn}</li>
-            ))}
-            <br/>
-            <a href= {/exercises/+{textID}}>
+            {questions[challengeNum-1]}
+            <br />
+            <br />
+            <a href= {/exercises/+textID+/challenges/+nextchallengeNum}>
+                <button>Next question</button>
+            </a>
+            <br />
+            <a href= {/exercises/+textID}>
                 <button>Back to the Text Page</button>
             </a>
         </div>
-    )
+    )}
+    else if(challengeNum <= 4){
+        console.log('Im here now');
+        const prevchallengeNum = parseInt(challengeNum) - 1;
+        const nextchallengeNum = parseInt(challengeNum) + 1;
+        return(
+        <div>
+            <h2>This is the challenges layout number {challengeNum} for text {textID}</h2>
+            <p>Right now, all you can do is see the questions</p>
+            <br/>
+            {questions[challengeNum-1]}
+            <br/>
+            <br />
+            <a href= {/exercises/+textID+/challenges/+prevchallengeNum}>
+                <button>Previous question</button>
+            </a>
+            <br />
+            <a href= {/exercises/+textID+/challenges/+nextchallengeNum}>
+                <button>Next question</button>
+            </a>
+            <br />
+            <a href= {/exercises/+textID}>
+                <button>Back to the Text Page</button>
+            </a>
+        </div>)
+    }
+
+    else{
+        const prevchallengeNum = parseInt(challengeNum) - 1;
+        return(
+        <div>
+            <h2>This is the challenges layout number {challengeNum} for text {textID}</h2>
+            <p>Right now, all you can do is see the questions</p>
+            <br/>
+            {questions[challengeNum-1]}
+            <br />
+            <br />
+            <a href= {/exercises/+textID+/challenges/+prevchallengeNum}>
+                <button>Previous question</button>
+            </a>
+            <br />
+            <a href= {/exercises/+textID}>
+                <button>Back to the Text Page</button>
+            </a>
+        </div>
+    )}
 }
 
 export default ChallengeLayout;
